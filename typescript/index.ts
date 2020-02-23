218658dcd54937a5cec9def1d0b81b77a6bcdaee
Lars Melchior <TheLartians@users.noreply.github.com>
2020-02-23 01:17:27 +0100

    run and check prettier

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,22 +1,26 @@
-
 function test() {
   // test bindings
   const a = new glue.lib.A(46);
   const b = new glue.lib.B(-5);
   b.setName("B");
-  console.assert(a.add(b).next().data() == 42);
+  console.assert(
+    a
+      .add(b)
+      .next()
+      .data() == 42
+  );
   console.assert(b.name() == "B");
   // test lua lib
-  const m = new Map<string,number>();
-  m.set("a",42);
+  const m = new Map<string, number>();
+  m.set("a", 42);
   console.assert(m.get("a") == 42);
-  print(`all tests passed`)
+  print(`all tests passed`);
 }
 
 class Greeter {
   private name: string;
 
-  constructor(name: string){
+  constructor(name: string) {
     this.name = name;
   }
 
