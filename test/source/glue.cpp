7ebee3567cfdee76571855a1af686778dd3aaf78
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-06 18:25:40 +0200

    update glue

--- test/source/glue.cpp
+++ test/source/glue.cpp
@@ -1,14 +1,14 @@
 #include <doctest/doctest.h>
 #include <glue/context.h>
-#include <glue/view.h>
 #include <greeter/glue.h>
 
 TEST_CASE("Glue bindings") {
   glue::Context context;
   auto greeterGlue = greeter::glue();
-  auto view = glue::View(greeterGlue);
   context.addRootMap(greeterGlue);
-  auto instance = context.createInstance(view["Greeter"][glue::keys::constructorKey]("Glue"));
-  REQUIRE(instance);
-  CHECK(instance["greet"](*view["LanguageCode"]["EN"]).as<std::string>() == "Hello, Glue!");
+  auto greeterInstance
+      = context.createInstance(greeterGlue["Greeter"][glue::keys::constructorKey]("Glue"));
+  REQUIRE(greeterInstance);
+  CHECK(greeterInstance["greet"](greeterGlue["LanguageCode"]["EN"]).as<std::string>()
+        == "Hello, Glue!");
 }
